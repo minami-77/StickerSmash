da0d524c11fa5d761cfa6dbca8932343c3fb123d
minami takayama <142367451+minami-77@users.noreply.github.com>
2026-06-21 22:45:25 +0900

    end of 1st chapter

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello Expo.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello Expo.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e",
+  },
+  text: {
+    color: "#fff",
+  },
+});
