079fd8ee15c5d2c9a6924b8be1cd7b301ee4fde4
minami takayama <142367451+minami-77@users.noreply.github.com>
2026-07-07 17:00:15 +0900

    change status bar color

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,26 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="(tabs)"
-        options={{
-          // Hide the header for the tabs layout
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="+not-found"
-        // No need to add options here, as the +not-found.tsx file already has a Stack.Screen with the desired options.
-        options={{}}
-      />
-    </Stack>
+    <>
+      {/* Change the status bar style to light */}
+      <StatusBar style="light" />
+
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            // Hide the header for the tabs layout
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="+not-found"
+          // No need to add options here, as the +not-found.tsx file already has a Stack.Screen with the desired options.
+          options={{}}
+        />
+      </Stack>
+    </>
   );
 }
