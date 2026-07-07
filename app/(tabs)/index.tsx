a0d950f7bd96336f20a5f8593192e1fc9a0e65c5
minami takayama <142367451+minami-77@users.noreply.github.com>
2026-07-07 17:24:10 +0900

    commented out some lines

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,16 @@
 import { Image } from "expo-image";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 const PlaceholderImage = require("../../assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Image source={PlaceholderImage} style={styles.image} />
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
 
-      <Text style={styles.text}>Hello StickerSmash!</Text>
+      {/* <Text style={styles.text}>Hello StickerSmash!</Text> */}
       {/* Uncomment because we can go to the about screen via the tab bar */}
       {/* <Link href={"/about"} style={styles.button}>
         Go to About screen
@@ -20,21 +22,24 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    // justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#25292e",
   },
-  text: {
-    color: "#fff",
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
-  },
+  // text: {
+  //   color: "#fff",
+  // },
+  // button: {
+  //   fontSize: 20,
+  //   textDecorationLine: "underline",
+  //   color: "#fff",
+  // },
   image: {
     width: 320,
     height: 440,
     borderRadius: 18,
+  },
+  imageContainer: {
+    flex: 1,
   },
 });
