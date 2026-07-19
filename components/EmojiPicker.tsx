d7054a254bc38442b6acac62a2ae49a134686151
minami takayama <142367451+minami-77@users.noreply.github.com>
2026-07-19 13:14:51 +0900

    add styles

--- components/EmojiPicker.tsx
+++ components/EmojiPicker.tsx
@@ -22,3 +22,29 @@ export default function EmojiPicker({ isVisible, children, onClose }: Props) {
     </Modal>
   );
 }
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    height: "25%",
+    width: "100%",
+    backgroundColor: "#25292e",
+    borderTopRightRadius: 18,
+    borderTopLeftRadius: 18,
+    position: "absolute",
+    bottom: 0,
+  },
+  titleContainer: {
+    height: "16%",
+    backgroundColor: "#464C55",
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontSize: 16,
+    color: "#fff",
+  },
+});
